befae90c57aa1de2f944e175f1ab0c3f19b5a4ce
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 06:15:28 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,7 @@ def first_challenge
   }
 
   #your code here
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|flavor| flavor == "strawberry"}
 
 
   #remember to return your newly altered contacts hash!
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,6 +7,8 @@ def second_challenge
   }
 
   #code your solution here!
+  groceries.values.flatten
+  
 
   
 
