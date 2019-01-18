8469d0256a0cf976f3a2ae13c536c1c02f83e803
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 06:10:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,10 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors]
+  contacts["Freddy Mercury"].each do |key, value|
+    if key == :favorite_icecream_flavors
+      value.each do |flavor|
+        if 
 
 
   #remember to return your newly altered contacts hash!
